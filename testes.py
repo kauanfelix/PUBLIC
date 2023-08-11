7b6eb0b34f135415f09f3f7a4976d3d8fb682d40
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2023-08-11 01:26:13 -0300

    Atualizar o testes.py

--- testes.py
+++ testes.py
@@ -5,3 +5,24 @@ qr_code = qrcode_scanner(key='qrcode_scanner')
 qr_code
 if qr_code:
   st.write(qr_code) 
+
+
+
+
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+html_string = '''
+<h1>HTML string in RED</h1>
+
+<script language="javascript">
+  document.querySelector("h1").style.color = "red";
+  console.log("Streamlit runs JavaScript");
+  alert("Streamlit runs JavaScript");
+</script>
+'''
+
+components.html(html_string)  # JavaScript works
+
+st.markdown(html_string, unsafe_allow_html=True)
