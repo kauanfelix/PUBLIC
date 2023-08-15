e0daad908a1451f21e1532f0b5e0d30bd54d7a43
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2023-08-15 10:16:58 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,19 +1,26 @@
 import streamlit as st  
-from streamlit_qrcode_scanner import qrcode_scanner  
+from streamlit_qrcode_scanner import qrcode_scanner 
 
-qr_code = qrcode_scanner(key='qrcode_scanner')  
-qr_code
-if qr_code:
-  st.write(qr_code) 
+from PIL import Image
+from pyzbar.pyzbar import decode
+from pillow_heif import register_heif_opener
 
+tab1, tab2 = st.tabs(["CÓDDIGO BARRAS", "QRCODE"])
+with tab1:
+  register_heif_opener()
+  image = st.file_uploader(label="Upload photo here", label_visibility='hidden')
+  if image is not None:
+      imagem_aberta = Image.open(image)
+      imagem_RGB = imagem_aberta.convert('RGB')  # Convert to greyscale to enforce HEIC data
+      barcodes = decode(imagem_RGB)
+      for codigo_barras in barcodes:
+          st.code(codigo_barras)
+      if not barcodes:
+          st.warning("CÓDIGOS BARRAS NÃO ENCONTRADO")
 
+with tab2:
+  qr_code = qrcode_scanner(key='qrcode_scanner')  
+  qr_code
+  if qr_code:
+    st.code(qr_code) 
 
-
-
-import streamlit as st
-
-from streamlit_back_camera_input import back_camera_input
-
-image = back_camera_input()
-if image:
-    st.image(image)
