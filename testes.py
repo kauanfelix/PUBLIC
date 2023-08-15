350559c4e3161604766e3463659d7459d40a13f1
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2023-08-15 11:06:50 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -5,22 +5,29 @@ from streamlit_qrcode_scanner import qrcode_scanner
 # from pyzbar.pyzbar import decode
 # from pillow_heif import register_heif_opener
 
-tab1, tab2 = st.tabs(["CÓDDIGO BARRAS TESTES", "CÓDDIGO BARRAS "])
-# with tab1:
-#   register_heif_opener()
-#   image = st.file_uploader(label="Upload photo here", label_visibility='hidden')
-#   if image is not None:
-#       imagem_aberta = Image.open(image)
-#       imagem_RGB = imagem_aberta.convert('RGB')  # Convert to greyscale to enforce HEIC data
-#       barcodes = decode(imagem_RGB)
-#       for codigo_barras in barcodes:
-#           st.code(codigo_barras)
-#       if not barcodes:
-#           st.warning("CÓDIGOS BARRAS NÃO ENCONTRADO")
+tab1, tab2, tab3 = st.tabs(["CÓDDIGO BARRAS UPLOAD", "CÓDDIGO BARRAS CAMERA", "QRCODE"])
+with tab1:
+  from PIL import Image
+  from pyzbar.pyzbar import decode
+  from pillow_heif import register_heif_opener
+  image = st.file_uploader("Upload photo here", label_visibility='hidden')
+  def display_links(barcode):
+    st.header(f"Barcode: {barcode}")
+    if image is not None:
+    im = Image.open(image)
+    im_greyscale = im.convert('RGB')  # Convert to greyscale to enforce HEIC data
+    barcodes = decode(im_greyscale)
+    for barcode in barcodes:
+        display_links(barcode.data.decode())
+    if not barcodes:
+        st.warning("ERRO.")
 
 with tab2:
   qr_code = qrcode_scanner(key='CODE_128')  
-  qr_code
+  if qr_code:
+    st.code(qr_code) 
+with tab3:
+  qr_code = qrcode_scanner(key='qr_code')  
   if qr_code:
     st.code(qr_code) 
 
