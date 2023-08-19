449260161b51d57c569dfcc537e4a4c51235bfb6
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2023-08-19 13:19:49 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,33 +1,32 @@
 import streamlit as st  
 from streamlit_qrcode_scanner import qrcode_scanner 
+from PIL import Image
+from pyzbar.pyzbar import decode
+from pillow_heif import register_heif_opener
 
-# from PIL import Image
-# from pyzbar.pyzbar import decode
-# from pillow_heif import register_heif_opener
-
-tab1, tab2, tab3 = st.tabs(["CÓDDIGO BARRAS UPLOAD", "CÓDDIGO BARRAS CAMERA", "QRCODE"])
-with tab1:
+funcao = st.selectbox(label='Importar chavde de acesso por:', options=["CÓDDIGO BARRAS UPLOAD", "CÓDDIGO BARRAS CAMERA", "QRCODE"])
+if funcao == 'CÓDDIGO BARRAS UPLOAD':
   from PIL import Image
   from pyzbar.pyzbar import decode
   from pillow_heif import register_heif_opener
-  image = st.file_uploader("Upload photo here", label_visibility='hidden')
-  def display_links(barcode):
-    st.header(f"Barcode: {barcode}")
-    if image is not None:
-      im = Image.open(image)
-      im_greyscale = im.convert('RGB')  # Convert to greyscale to enforce HEIC data
-      barcodes = decode(im_greyscale)
-      for barcode in barcodes:
-          display_links(barcode.data.decode())
-    if not barcodes:
-        st.warning("ERRO.")
+  im_UPLOAD = st.file_uploader("Upload photo here", label_visibility='hidden', type=['png', 'jpeg', 'jpg'])
 
-with tab2:
-  qr_code = qrcode_scanner(key='CODE_128')  
-  if qr_code:
-    st.code(qr_code) 
-with tab3:
+  if im_UPLOAD is not None:
+    im_OPEN = Image.open(im_UPLOAD)
+    im_RGB = im_OPEN.convert('RGB')
+    st.image(im_OPEN)
+    barcodes = decode(im_RGB)
+    for barcode in barcodes:
+        st.code(barcode.data.decode())
+    if not barcodes:
+        st.warning("CODIGO BARRAS ILEGIVEL")
+
+if funcao == 'CÓDDIGO BARRAS CAMERA':
+  code_128 = qrcode_scanner(key='CODE_128')  
+  if code_128:
+    st.code(code_128)
+
+if funcao == 'QRCODE':
   qr_code = qrcode_scanner(key='qr_code')  
   if qr_code:
     st.code(qr_code) 
-
